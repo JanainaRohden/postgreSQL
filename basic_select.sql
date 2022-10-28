977b2e45f496602084cc4fdbd9062828fc31efb5
janaina <janainarohden@gmail.com>
2022-10-28 16:52:12 -0300

    Select basico na tabela customer.

---
+++ basic_select.sql
@@ -0,0 +1,23 @@
+----QUESTÃO 1, select básico
+
+CREATE TABLE customers (
+	id NUMERIC PRIMARY KEY,
+	name varchar,
+	street varchar,
+	city varchar,
+	state char(2),
+	credit_limit numeric
+);
+INSERT INTO customers (id,name,street,city,state,credit_limit)
+VALUES 
+(1,'Pedro Augusto da Rocha','Rua Pedro Carlos Hoffman','Porto Alegre','RS',700.00),
+(2,'Antonio Carlos Mamel','Av Pinheiros','Belo Horizonte','MG',3500.50),
+(3,'Luiza Augusta Mhor','Rua Salto Grande','Niteroi','RJ',4000.00),
+(4,'Jane Ester','Av 7 de Setembro','Erechim','RS',800.00),
+(5,'Marcos Antônio dos Santos','Av Farrapos','Porto Alegre','RS',4250.25);
+
+SELECT name
+FROM customers 
+WHERE state = 'RS';
+
+DROP TABLE customers;
